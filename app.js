d8071d44f1e67ad8f0e78e38e70630ad424beef1
eoriwech <prakash_gurung@hotmail.co.uk>
2023-10-30 10:38:59 +0000

    feat: added function to carry out subtraction

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 //performs addition of two numbers
 function add(num1, num2) {
-    let sum = num1 + num2;
-    return sum;
+    let result = num1 + num2;
+    return result;
+}
+
+//Subtraction of two numbers
+function subtract(num1, num2) {
+    let result = num1 - num2;
+    return result;
+}
+
+//Multiplication of two numbers
+function multiply(num1, num2) {
+    let result = num1 * num2;
+    return result;
 }
