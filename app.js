767fedff85184e838e4ff035c89ce01d75442b5a
eoriwech <prakash_gurung@hotmail.co.uk>
2023-11-04 14:37:33 +0000

    fix: changed the initial variable names

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-let firstNum = null;
-let secondNum = null;
-let operatorSign = null;
+let finalValue = null;
+let nextNum = null;
+let currentOperator = null;
 let displayValue = "";
 
 //performs addition of two numbers
@@ -33,18 +33,94 @@ function operate(operator, num1, num2) {
         return add(num1, num2);
     } else if (operator == "-") {
         return subtract(num1, num2);
-    } else if (operator == "*") {
+    } else if (operator === "x") {
         return multiply(num1, num2);
     } else if (operator == "/") {
-        return divide(num1, num2);
+        if (num2 == 0) {
+            return "Math error, cannot divide by 0";
+        } else {
+            return divide(num1, num2);
+        }
     }
 }
 
-//event listener
-let display = document.querySelector(".display-container");
-document.querySelectorAll("button").forEach((item) => {
-    item.addEventListener("click", (e) => {
+function rounding(numToRound) {
+    let roundingPlace = 100000;
+    return Math.round(numToRound * roundingPlace) / roundingPlace;
+}
+
+//event Listeners
+
+let displayInput = document.querySelector(".display-input");
+let displayOutput = document.querySelector(".display-output");
+let operatorButton = document.querySelectorAll(".operator-btn");
+let resultButton = document.querySelector(".result-btn");
+let clearButton = document.querySelector(".clear-btn");
+let delButton = document.querySelector(".del-btn");
+
+//event listener to display the number when relevant button is pressed
+document.querySelectorAll(".number-btn").forEach((item) => {
+    item.addEventListener('click', (e) => {
         displayValue += `${e.target.innerText}`;
-        display.textContent = displayValue;
+        displayInput.textContent = displayValue;
     })
+})
+
+//event listener to display the operator sign when pressed
+operatorButton.forEach((item) => {
+    item.addEventListener('click', (e) => {
+        displayValue += ` ${e.target.innerText} `;
+        displayInput.textContent = displayValue;
+        let displayValueArray = displayValue.split(" ");
+        if (displayValueArray[2] != "") {
+            finalValue = parseFloat(displayValueArray[0]);
+            currentOperator = displayValueArray[1];
+            nextNum = parseFloat(displayValueArray[2])
+            finalValue = operate(currentOperator, finalValue, nextNum);
+            displayOutput.textContent = finalValue;
+            displayValueArray[0] = `${finalValue}`;
+            displayValueArray.splice(1, 2)
+            displayValue = displayValueArray.join(" ");
+        }
+        //rounding up the final value up to 5 decimal places
+        finalValue = rounding(finalValue);
+        
+        displayOutput.textContent = finalValue;
+    })
+})
+
+//event listener to carry out the calculation when equal button is pressed
+//the value of the variable 'displayValue' is split into array
+//the operate function is then called with the resulting value of the variables
+resultButton.addEventListener('click', () => {
+    let displayValueArray = displayValue.split(" ");
+    finalValue = parseFloat(displayValueArray[0]);
+
+    //loop through odd index number position where operators are expected
+    //assign the next number which is one higher than operator index position
+    //evalute the result and assign it to the final value
+    for (let i = 1; i < displayValueArray.length; i += 2) {
+        currentOperator = displayValueArray[i];
+        nextNum = parseFloat(displayValueArray[i + 1])
+        finalValue = operate(currentOperator, finalValue, nextNum);
+    }
+ 
+    //rounding up the final value up to 5 decimal places
+    finalValue = rounding(finalValue);
+
+    displayOutput.textContent = finalValue;
+})
+
+//event listener to remove all the characters in the calculator display 
+clearButton.addEventListener('click', () => {
+    displayValue = "";
+    displayInput.textContent = displayValue;
+    displayOutput.textContent = "";
+    
+})
+
+//event listener to remove the last character in the display screen
+delButton.addEventListener('click', () => {
+    displayValue = displayValue.slice(0, displayValue.length -1);
+    displayInput.textContent = displayValue;
 })
