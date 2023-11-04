8833fc7b2550b564986e0cb3112a0d562226c74b
eoriwech <prakash_gurung@hotmail.co.uk>
2023-11-04 14:38:40 +0000

    feat: added function to round number to 5 d.p.

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ function operate(operator, num1, num2) {
     }
 }
 
+//function to round number to nearest 5 decimal places
 function rounding(numToRound) {
     let roundingPlace = 100000;
     return Math.round(numToRound * roundingPlace) / roundingPlace;
@@ -66,61 +67,3 @@ document.querySelectorAll(".number-btn").forEach((item) => {
     })
 })
 
-//event listener to display the operator sign when pressed
-operatorButton.forEach((item) => {
-    item.addEventListener('click', (e) => {
-        displayValue += ` ${e.target.innerText} `;
-        displayInput.textContent = displayValue;
-        let displayValueArray = displayValue.split(" ");
-        if (displayValueArray[2] != "") {
-            finalValue = parseFloat(displayValueArray[0]);
-            currentOperator = displayValueArray[1];
-            nextNum = parseFloat(displayValueArray[2])
-            finalValue = operate(currentOperator, finalValue, nextNum);
-            displayOutput.textContent = finalValue;
-            displayValueArray[0] = `${finalValue}`;
-            displayValueArray.splice(1, 2)
-            displayValue = displayValueArray.join(" ");
-        }
-        //rounding up the final value up to 5 decimal places
-        finalValue = rounding(finalValue);
-        
-        displayOutput.textContent = finalValue;
-    })
-})
-
-//event listener to carry out the calculation when equal button is pressed
-//the value of the variable 'displayValue' is split into array
-//the operate function is then called with the resulting value of the variables
-resultButton.addEventListener('click', () => {
-    let displayValueArray = displayValue.split(" ");
-    finalValue = parseFloat(displayValueArray[0]);
-
-    //loop through odd index number position where operators are expected
-    //assign the next number which is one higher than operator index position
-    //evalute the result and assign it to the final value
-    for (let i = 1; i < displayValueArray.length; i += 2) {
-        currentOperator = displayValueArray[i];
-        nextNum = parseFloat(displayValueArray[i + 1])
-        finalValue = operate(currentOperator, finalValue, nextNum);
-    }
- 
-    //rounding up the final value up to 5 decimal places
-    finalValue = rounding(finalValue);
-
-    displayOutput.textContent = finalValue;
-})
-
-//event listener to remove all the characters in the calculator display 
-clearButton.addEventListener('click', () => {
-    displayValue = "";
-    displayInput.textContent = displayValue;
-    displayOutput.textContent = "";
-    
-})
-
-//event listener to remove the last character in the display screen
-delButton.addEventListener('click', () => {
-    displayValue = displayValue.slice(0, displayValue.length -1);
-    displayInput.textContent = displayValue;
-})
