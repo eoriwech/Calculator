040fd4d9394043ce037118fae9b6238686c1988e
eoriwech <prakash_gurung@hotmail.co.uk>
2023-11-06 11:34:14 +0000

    feat: added more comments for context and changed the variable name 'finalValue' to 'totalValue'

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let finalValue = null;
+let totalValue = null;
 let nextNum = null;
 let currentOperator = null;
 let displayValue = "";
@@ -72,21 +72,21 @@ document.querySelectorAll(".number-btn").forEach((item) => {
 //the operate function is then called with the resulting value of the variables
 resultButton.addEventListener('click', () => {
     let displayValueArray = displayValue.split(" ");
-    finalValue = parseFloat(displayValueArray[0]);
+    totalValue = parseFloat(displayValueArray[0]);
 
     //loop through odd index number position where operators are expected
     //assign the next number which is one higher than operator index position
-    //evalute the result and assign it to the final value
+    //evalute the result and assign it to the total value
     for (let i = 1; i < displayValueArray.length; i += 2) {
         currentOperator = displayValueArray[i];
         nextNum = parseFloat(displayValueArray[i + 1])
-        finalValue = operate(currentOperator, finalValue, nextNum);
+        totalValue = operate(currentOperator, totalValue, nextNum);
     }
  
-    //rounding up the final value up to 5 decimal places
-    finalValue = rounding(finalValue);
+    //rounding up the total value up to 5 decimal places
+    totalValue = rounding(totalValue);
 
-    displayOutput.textContent = finalValue;
+    displayOutput.textContent = totalValue;
 })
 
 //event listener to display the operator sign when pressed
@@ -95,20 +95,27 @@ operatorButton.forEach((item) => {
         displayValue += ` ${e.target.innerText} `;
         displayInput.textContent = displayValue;
         let displayValueArray = displayValue.split(" ");
+        
+        //if the third value exist, which makes a pair then evaluate them
         if (displayValueArray[2] != "") {
-            finalValue = parseFloat(displayValueArray[0]);
+
+            //assigning numbers to evalute based on the array created by splitting up the current display values
+            totalValue = parseFloat(displayValueArray[0]);
             currentOperator = displayValueArray[1];
             nextNum = parseFloat(displayValueArray[2])
-            finalValue = operate(currentOperator, finalValue, nextNum);
-            displayOutput.textContent = finalValue;
-            displayValueArray[0] = `${finalValue}`;
+            totalValue = operate(currentOperator, totalValue, nextNum);
+
+            //after the first pair is evaluated, assign resulting total value to the first term in display array
+            //remove the remaining terms so that new number and operator can take their place
+            displayOutput.textContent = totalValue;
+            displayValueArray[0] = `${totalValue}`;
             displayValueArray.splice(1, 2)
             displayValue = displayValueArray.join(" ");
         }
-        //rounding up the final value up to 5 decimal places
-        finalValue = rounding(finalValue);
+        //rounding up the total value up to 5 decimal places
+        totalValue = rounding(totalValue);
         
-        displayOutput.textContent = finalValue;
+        displayOutput.textContent = totalValue;
     })
 })
 
